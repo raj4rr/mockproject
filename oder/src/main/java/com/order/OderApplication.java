42a254273fce498c4462fabc805cc4a720fda9c8
Rajesh Kumar <raj4rr@gmail.com>
2024-10-10 13:55:37 +0530

    eureka client added

--- oder/src/main/java/com/order/OderApplication.java
+++ oder/src/main/java/com/order/OderApplication.java
@@ -2,10 +2,11 @@ package com.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class OderApplication {
 
 	public static void main(String[] args) {
